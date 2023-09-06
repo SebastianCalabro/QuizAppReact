ec4889b44e4ecf06b68d9854eea55bc173a58479
SebastianCalabro <sebastianjoaquincalabro@gmail.com>
2023-09-05 21:36:56 -0300

    update-finishing styles

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,21 @@
 import s from "./Home.module.css"
 import Cards from "../Cards/Cards.jsx"
 import Profile from "../Profile/Profile"
+
 const Home = ()=>{
+
+    /* const [questions, setQuestions] = useState([]) */
+
+    /* useEffect(() => {
+        const fetchQuestions = async () => {
+            const response = await fetch(`https://quiz-7.com/categories.json`)
+            const data = await response.json()
+            setQuestions(data)
+        }
+        fetchQuestions()
+    }, [])
+    console.log(questions) */
+
     return(
         <div className={s.box}>
             <Profile/>
