e1f32a708763a8b956625402d3c734eae839ad68
SebastianCalabro <sebastianjoaquincalabro@gmail.com>
2023-09-06 19:40:11 -0300

    update-quiz functionality

--- src/components/QuizOptions/QuizOptions.jsx
+++ src/components/QuizOptions/QuizOptions.jsx
@@ -1,40 +1,33 @@
 import s from "./QuizOptions.module.css"
 import { useState } from "react"
-const QuizOptions = ({randomOptions})=>{
+import { AiOutlineCheck } from "react-icons/ai"
+import {RxCross1} from "react-icons/rx"
+const QuizOptions = ({score, setScore, randomOptions, order, setOrder})=>{
 
     const [selectedOption, setSelectedOption] = useState()
 
-    const handle = (optionPicked)=>{
-        if(optionPicked.split("").includes("*")){
-            setSelectedOption(optionPicked)
-        console.logc
-        }else{
-            setSelectedOption(optionPicked)
-        console.log("opciooon",optionPicked)
+    const hanldeNext = ()=>{
+        if(selectedOption.split("").includes("*")){
+            setScore(score+100)
         }
-        
+        setOrder(order+1)
+        setSelectedOption(false)
     }
 
         let correctOption = randomOptions.filter(op=>op.split("").includes("*")).join("")
-        /* for (let i = 0; i < randomOptions.length; i++) {
-            let opArr = randomOptions[i].split("")
-            if(opArr.includes("*")){
-                correctOption = opArr.join("")
-            }  
-        } */
-        /* randomOptions.slice(0,5)
-        console.log(randomOptions.length) */
-        console.log(randomOptions)
     return(
         <div className={s.options_box}>
                {    
                     randomOptions.map((op, i)=>
-                        <button key={i} disabled={selectedOption} className={selectedOption===op?op===correctOption?s.correct:s.incorrect:s.option} onClick={()=>handle(op)}>
+                        <button key={i} disabled={selectedOption} className={selectedOption?op!==correctOption&&op===selectedOption?s.incorrect:op===correctOption&&selectedOption?s.correct:s.after_option:s.option} onClick={()=>setSelectedOption(op)}>
                             {op===correctOption?op.split("").slice(0, -1).join(""):op}
-                            <div className={s.circle}></div>
+                            {selectedOption?op!==correctOption&&op===selectedOption?<div className={s.incorrect_circle}><RxCross1 className={s.icons}/></div>:op===correctOption&&selectedOption?<div className={s.correct_circle}><AiOutlineCheck className={s.icons}/></div>:<div className={s.circle}></div>:<div className={s.circle}></div>}
+                            {/* <div className={s.correct_circle}><AiOutlineCheck/></div>
+                            <div className={s.incorrect_circle}><RxCross1/></div> */}
+                            
                         </button>
                     )}
-                <button className={selectedOption?s.next_button:s.non_exist}>Next</button>
+                <button onClick={()=>hanldeNext()} className={selectedOption?s.next_button:s.non_exist}>Next</button>
             </div>
     )
 }
