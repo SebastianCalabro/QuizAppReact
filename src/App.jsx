417a3bd8c1ce154ecfe52ced02a861ed02151727
SebastianCalabro <sebastianjoaquincalabro@gmail.com>
2023-09-05 21:36:00 -0300

    update- Routes for App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from "./components/Home/Home.jsx"
+import Quiz from './components/Quiz/Quiz.jsx'
 
 import './App.css'
 
 function App() {
-
+  
   return (
       <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/question/:questionId" element={<Quiz />}/>
       </Routes>
     </BrowserRouter>
   )
