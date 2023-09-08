9c75e35f6ca0688855b72be3bb926a09b0bfd1df
SebastianCalabro <sebastianjoaquincalabro@gmail.com>
2023-09-08 02:21:41 -0300

    update - optimizing randomize function

---
+++ src/utils/utils.js
@@ -0,0 +1,12 @@
+export const randomize = (array)=> {
+    var i = array.length,
+        j = 0,
+        temp;
+    while (i--) {
+        j = Math.floor(Math.random() * (i+1));
+        temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
